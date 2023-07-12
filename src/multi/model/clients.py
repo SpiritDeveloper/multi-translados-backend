345d957f0ec34c54b71815b249bae4de38f5289a
Fernando Cortés <fernando.cortes971@gmail.com>
2023-07-11 22:04:26 -0600

    Cambios en el modelo de clientes

--- src/multi/model/clients.py
+++ src/multi/model/clients.py
@@ -19,6 +19,7 @@ class Clients(db.Model):
     neighborhood    = Column(String(255), nullable=False)
     state           = Column(String(255), nullable=False)
     zip_code        = Column(String(5), nullable=False)
+    logo            = Column(String(255), nullable=False)
     #? https://stackoverflow.com/questions/13370317/sqlalchemy-default-datetime
     startedAt = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
     updatedAt = Column(DateTime(timezone=True), nullable=False, onupdate=func.now())
