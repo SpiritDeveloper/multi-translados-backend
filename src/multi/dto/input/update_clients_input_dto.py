c6c0c9ebb2bf7f8d9b33b6d15219ef9711b52c77
Fernando Cortés <fernando.cortes971@gmail.com>
2023-08-07 20:31:28 -0600

    update clients input changed

--- src/multi/dto/input/update_clients_input_dto.py
+++ src/multi/dto/input/update_clients_input_dto.py
@@ -2,10 +2,19 @@ from marshmallow import Schema, ValidationError, fields
 
 
 class updateClientInputSchema(Schema):
-    id                = fields.UUID(required=True, description="Client uuid") 
-    name                = fields.Str(required=False, description="Client name")
-    description  = fields.Str(required=False, description="Client description")
-    active  = fields.Bool(required=False, description="Client status")
+    id              = fields.UUID(required=True, description="Client uuid")
+    code            = fields.Str(required=False, description="Client code")
+    business_name   = fields.Str(required=False, description="Client business name")
+    rfc             = fields.Str(required=False, description="Client RFC")
+    street_name     = fields.Str(required=False, description="Client street name")
+    phone_number    = fields.Str(required=False, description="Client phone number")
+    interior_number = fields.Str(required=False, description="Client interior number")
+    exterior_number = fields.Str(required=False, description="Client exterior number")
+    neighborhood    = fields.Str(required=False, description="Client neighborhood")
+    state           = fields.Str(required=False, description="Client state")
+    zip_code        = fields.Str(required=False, description="Client zip code")
+    logo            = fields.Str(required=False, description="Client logo")
+    active          = fields.Bool(required=False, description="Client status")
 
     class Meta:
         ordered = True
