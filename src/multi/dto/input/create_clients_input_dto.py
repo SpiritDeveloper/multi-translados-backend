00cb32038835a4c630b5438711d2570244acd6f9
Fernando Cortés <fernando.cortes971@gmail.com>
2023-08-08 19:56:50 -0600

    validations create_client_input_dto added

--- src/multi/dto/input/create_clients_input_dto.py
+++ src/multi/dto/input/create_clients_input_dto.py
@@ -1,16 +1,16 @@
-from marshmallow import Schema, ValidationError, fields
+from marshmallow import Schema, ValidationError, fields, validate
 
 class createClientInputSchema(Schema):
   code            = fields.Str(required=True, description="Client code")
   business_name   = fields.Str(required=True, description="Client business name")
-  rfc             = fields.Str(required=True, description="Client RFC")
+  rfc             = fields.Str(required=True, validate=validate.Length(min=12, max=13), description="Client RFC")
   street_name     = fields.Str(required=True, description="Client street name")
-  phone_number    = fields.Str(required=True, description="Client phone number")
-  interior_number = fields.Str(required=True, description="Client interior number")
-  exterior_number = fields.Str(required=True, description="Client exterior number")
+  phone_number    = fields.Str(required=True, validate=validate.Length(max=15), description="Client phone number")
+  interior_number = fields.Str(required=True, validate=validate.Length(max=4), description="Client interior number")
+  exterior_number = fields.Str(required=True, validate=validate.Length(max=4), description="Client exterior number")
   neighborhood    = fields.Str(required=True, description="Client neighborhood")
   state           = fields.Str(required=True, description="Client state")
-  zip_code        = fields.Str(required=True, description="Client zip code")
+  zip_code        = fields.Str(required=True, validate=validate.Length(equal=5), description="Client zip code")
   logo            = fields.Str(required=True, description="Client logo")
 
   class Meta:
